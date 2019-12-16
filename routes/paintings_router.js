6bd3ede34552552dfa33b195bd7f462bcbd1775e
Ivan <ifrancesalcantara@gmail.com>
2019-12-16 13:30:45 +0100

    deployment attempt 2

--- routes/paintings_router.js
+++ routes/paintings_router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Paintings = require("../models/painting");
-const User = require("../models/user");
+const User = require("../models/User");
 const multer = require("multer");
 const upload = multer({ dest: "./../public/images/uploads/" });
 const parser = require("./../config/cloudinary");
