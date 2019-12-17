f30112755886041cfa41817269e7d7d9d3567874
Ivan <ifrancesalcantara@gmail.com>
2019-12-17 14:05:31 +0100

    solved heroku

--- routes/paintings_router.js
+++ routes/paintings_router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Paintings = require("../models/painting");
 const User = require("../models/user");
-const multer = require("multer");
-const upload = multer({ dest: "./../public/images/uploads/" });
 const parser = require("./../config/cloudinary");
 
 // /paintings/image
@@ -12,15 +10,13 @@ router.post("/image", parser.single("photo"), (req, res, next) => {
     next(new Error("No file uploaded!"));
   }
   const imageUrl = req.file.secure_url;
-  console.log(imageUrl);
   res.json(imageUrl).status(200);
 });
 
-router.post("/", upload.single("photo"), function(req, res, next) {
+router.post("/", function(req, res, next) {
   const { creator } = req.body;
   Paintings.create(req.body)
     .then(newPainting => {
-      console.log(newPainting);
       res.status(202).json(newPainting);
       User.findByIdAndUpdate(
         creator,
