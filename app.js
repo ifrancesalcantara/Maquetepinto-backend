ef0d90ca4100e4f9b9eeb0927fdad26e19f470aa
Ivan <ifrancesalcantara@gmail.com>
2019-12-18 15:14:05 +0100

    barra

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
+require("dotenv").config();
 
-const auth = require('./routes/auth');
-const usersRouter = require('./routes/users_router');
-const paintingsRouter = require('./routes/paintings_router');
-const chatRouter = require('./routes/chat_router');
-
+const auth = require("./routes/auth");
+const usersRouter = require("./routes/users_router");
+const paintingsRouter = require("./routes/paintings_router");
+const chatRouter = require("./routes/chat_router");
 
 // MONGOOSE CONNECTION
 mongoose
   .connect(process.env.MONGODB_URI, {
     keepAlive: true,
     useNewUrlParser: true,
-    reconnectTries: Number.MAX_VALUE,
+    reconnectTries: Number.MAX_VALUE
   })
-  .then( (x) => console.log(`Connected to database ${x.connections[0].name}`))
-  .catch( (err) => {
-    console.log("THE ERROR IS HEREEEEE")
-    console.error(err)});
-
+  .then(x => console.log(`Connected to database ${x.connections[0].name}`))
+  .catch(err => {
+    console.log("THE ERROR IS HEREEEEE");
+    console.error(err);
+  });
 
 // EXPRESS SERVER INSTANCE
 const app = express();
 
-
 // CORS MIDDLEWARE SETUP
 app.use(
-  cors(
-  //   {
-  //   credentials: true,
-  //   origin: [process.env.PUBLIC_DOMAIN, "https://paintapop.herokuapp.com"],
-  // }
-  ),
+  cors({
+    credentials: true,
+    origin: [process.env.PUBLIC_DOMAIN, "https://paintapop.herokuapp.com/"]
+  })
 );
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -54,30 +50,29 @@ app.use(
   session({
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60, // 1 day
+      ttl: 24 * 60 * 60 // 1 day
     }),
     secret: process.env.SECRET_SESSION,
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  }),
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
 );
 
 // MIDDLEWARE
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTER MIDDLEWARE
-app.use('/auth', auth);
-app.use("/users", usersRouter)
-app.use("/paintings", paintingsRouter)
-app.use("/chat", chatRouter)
+app.use("/auth", auth);
+app.use("/users", usersRouter);
+app.use("/paintings", paintingsRouter);
+app.use("/chat", chatRouter);
 
 // ROUTE FOR SERVING REACT APP (index.html)
 app.use((req, res, next) => {
@@ -88,19 +83,18 @@ app.use((req, res, next) => {
 // ERROR HANDLING
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404).json({ code: 'not found' });
+  res.status(404).json({ code: "not found" });
 });
 
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error("ERROR", req.method, req.path, err);
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    const statusError = err.status || '500';
+    const statusError = err.status || "500";
     res.status(statusError).json(err);
   }
 });
-
 
 module.exports = app;
